f22ac3f180f1ec9f9347a933ad99e908ec9ec06d
Sam Schrader <samuelmws@gmail.com>
2025-01-16 11:11:22 -0800

    added colors

--- sketch.js
+++ sketch.js
@@ -12,26 +12,28 @@ function draw() {
   translate(75,-150);
   rectMode(CENTER);
   rect(200, 200, 175, 120);
-  fill(255);
+  fill(150, 228, 250);
   arc(x/2, y/2-45, 175, 175, PI, 0); // top hemisphere
   arc(x/2, y/2+45, 175, 175, 0, PI); // bottom hemisphere
   
   push();
-    fill(0);
+    noStroke();
+    fill(106, 143, 4);
     //arc(x/2, y/2-45, 170, 170, PI, 0); // top negative hemisphere
     arc(x/2, y/2+45, 170, 170, 0, PI); // bottom negative hemisphere
   pop();
 
   push();
-  fill(0);
-  triangle(x/4+15, y/2+45, 285, 140, 285, 245); // bottom triangle
+    noStroke();
+    fill(106, 143, 4);
+    triangle(x/4+15, y/2+45, 285, 140, 285, 245); // bottom triangle
   pop();
 
-  push();
-  fill(0);
-  translate(0, 13);
-  triangle(174, 182, 178, 150, 205, 163);
-  pop();
+  // push();
+  // fill(0);
+  // translate(0, 13);
+  // triangle(174, 182, 178, 150, 205, 163);
+  // pop();
 
   print(mouseX, mouseY);
 }
