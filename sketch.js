6e8f89033512a62fd47ccb247fb74906e99fd12c
Sam Schrader <samuelmws@gmail.com>
2025-01-16 10:22:21 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+
+  var x = width;
+  var y = height;
+
+  rotate(0.610865); // a bit less than 45 degrees
+  translate(75,-150);
+  rectMode(CENTER);
+  rect(200, 200, 175, 120);
+  fill(255);
+  arc(x/2, y/2-45, 175, 175, PI, 0); // top hemisphere
+  arc(x/2, y/2+45, 175, 175, 0, PI); // bottom hemisphere
+  
+  push();
+    fill(0);
+    //arc(x/2, y/2-45, 170, 170, PI, 0); // top negative hemisphere
+    arc(x/2, y/2+45, 170, 170, 0, PI); // bottom negative hemisphere
+  pop();
+
+  push();
+  fill(0);
+  triangle(x/4+15, y/2+45, 285, 140, 285, 245); // bottom triangle
+  pop();
+
+  push();
+  fill(0);
+  translate(0, 13);
+  triangle(174, 182, 178, 150, 205, 163);
+  pop();
+
+  print(mouseX, mouseY);
+}
